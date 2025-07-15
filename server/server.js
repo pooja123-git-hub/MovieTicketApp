e0f61782ef69ac098ce3222bb7af7a5d016d6cca
Pooja <dwivedipooja732@gmail.com>
2025-07-15 10:37:15 +0530

    "change server file"

--- server/server.js
+++ server/server.js
@@ -1,25 +1,26 @@
 import express from 'express';
 import cors from 'cors';
-
-import 'dotenv/config'
+import 'dotenv/config.js';
 import connectDb from './configs/db.js';
-import { clerkMiddleware } from '@clerk/express'
+import { clerkMiddleware } from '@clerk/express';
 import { serve } from "inngest/express";
-import { inngest, functions } from "./inngest/index.js"
-const app=express();
-const port=3000;
-await connectDb();
-//middleware
+import { inngest, functions } from "./inngest/index.js";
+
+const app = express();
+
+// Middleware
 app.use(express.json());
 app.use(cors());
-app.use(clerkMiddleware())
-app.get('/',(req,res)=>{
-    res.send("Radhey Radhey")})
+app.use(clerkMiddleware());
+
+
+app.get('/', (req, res) => {
+  res.send("Radhey Radhey");
+});
+
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
 
+connectDb();
 
-    app.listen(port,()=>{
-        console.log(`Server  listening at http://localhost:${port}`);
-        
-    })
+export default app;
