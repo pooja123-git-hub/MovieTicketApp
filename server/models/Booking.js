a6f05f0be74ca7b2c98314565002a91142f97742
Pooja <dwivedipooja732@gmail.com>
2025-08-04 12:08:01 +0530

    booking

--- server/models/Booking.js
+++ server/models/Booking.js
@@ -6,7 +6,7 @@ const bookingSchema=new mongoose.Schema({
     amount:{type:Number,required:true},
     bookingSeats:{type:Array,required:true},
     isPaid:{type:Boolean,default:false},
-    paymentsLink:{type:String},
+    paymentLink:{type:String},
 
 },{timestamps:true})
 const Booking =mongoose.model("Booking",bookingSchema);
